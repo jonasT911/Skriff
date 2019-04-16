427aba26ef91dbe41030aad673c4f8accec3b486
jonasT911 <37782575+jonasT911@users.noreply.github.com>
2019-04-16 11:27:30 -0500

    Save System

---
+++ Game.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+
+public class Game 
+{
+
+    public static Game current;
+    public string currentScene;
+
+    public Game()
+    {
+        currentScene = "";
+        currentScene = SceneManager.GetActiveScene().name;
+    }
+ 
+}
---
+++ SaveGame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class SaveGame 
+{
+    
+    public static Game savedGames = new Game();
+    
+    public static void Save()
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/savedGames.gd");
+        bf.Serialize(file, currentScene);
+        file.Close();
+    }
+
+    public static void Load()
+    {
+      //  string nextLevel = "error";
+        if (File.Exists(Application.persistentDataPath + "/savedGames.gd"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+           string destination = (string)bf.Deserialize(file);
+         
+            file.Close();
+
+         
+            
+            SceneManager.LoadScene(destination);
+        }
+
+      //  return nextLevel ;
+    }
+}
